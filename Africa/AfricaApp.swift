0225331cbb6c67567dba83ba7c73a1a534bad166
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-05-03 18:52:38 -0400

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Carlos Del Carpio on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
